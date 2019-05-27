4ec2ff55c512a18c8acf3911883be13fc28673b8
teo <teo@DESKTOP-2577AS1>
2019-05-27 20:54:40 +0300

    some changes

---
+++ src/main/java/threeaddr/ReturnInstr.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threeaddr;
+
+
+public class ReturnInstr implements Instruction{
+
+    private String value;
+
+    public ReturnInstr() {
+    }
+
+    public ReturnInstr(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    
+    @Override
+    public String emit() {
+        if(value.isEmpty())
+            return "return";
+        else
+            return "return " + value;   
+    }
+    
+}
